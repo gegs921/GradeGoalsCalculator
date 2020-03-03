fc29e08e6d010d3f53a7902ded76ddc3f8576d47
talonbragg <w1tchhunt@protonmail.com>
2020-03-02 19:46:09 -0800

    form validation for login

---
+++ public/js/lib/passEmailValidation.js
@@ -0,0 +1,35 @@
+function validateEmail(email) {
+  if(typeof(email) !== 'string') {
+    return console.log('email should be a string');
+    return false;
+  }
+  stringArr = email.split('@');
+  if(stringArr.length < 2 || stringArr[1].split('.').length < 2) {
+    console.log('not valid');
+    return false;
+  }
+  else {
+    console.log('valid');
+    return true
+  }
+}
+
+function validatePassword(pass) {
+  charArr = pass.split('');
+  if(charArr.length < 8) {
+    console.log('not valid');
+    return false;
+  }
+  else {
+    for(let i = 0; i < charArr.length; i++) {
+      if(isNaN(charArr[i]) === false) {
+        console.log('valid');
+        return true;
+      }
+      else if(i === charArr.length - 1 && isNaN(charArr[i]) === true) {
+        console.log('not valid');
+        return false;
+      }
+    }
+  }
+}
--- public/js/login.js
+++ public/js/login.js
@@ -6,16 +6,24 @@ let app = new Vue({
   },
   methods: {
     post: function() {
-      axios.post('/loginComplete', {
-        email: this.email,
-        password: this.password
-      }).then((res) => {
-        console.log(res);
-        window.location.href = "/";
+      if(validateEmail(this.email) === false) {
+        return;
+      }
+      else if(validatePassword(this.password) === false){
+        return;
+      }
+      else {
+        axios.post('/loginComplete', {
+          email: this.email,
+          password: this.password
+        }).then((res) => {
+          console.log(res);
+          window.location.href = "/";
 
-      }).catch((err) => {
-        console.log(err);
-      });
+        }).catch((err) => {
+          console.log(err);
+        });
+      }
     }
   }
 })
