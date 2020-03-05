8181a764a6a84078234766daa0750492d33b6d22
talonbragg <w1tchhunt@protonmail.com>
2020-03-04 21:41:12 -0800

    password and email validation visual

--- public/js/login.js
+++ public/js/login.js
@@ -2,14 +2,27 @@ let app = new Vue({
   el: '#app',
   data: {
     email: '',
-    password: ''
+    password: '',
+    errorText: '',
+    errorType: '',
+    errorShown: false,
+    errorTypes: {
+      emailError: 1,
+      passwordError: 2
+    },
   },
   methods: {
     post: function() {
       if(validateEmail(this.email) === false) {
+        this.errorType = this.errorTypes.emailError;
+        this.errorText = "Email is not valid!";
+        this.errorShown = true;
         return;
       }
       else if(validatePassword(this.password) === false){
+        this.errorType = this.errorTypes.passwordError;
+        this.errorText = "Password is not valid";
+        this.errorShown = true;
         return;
       }
       else {
